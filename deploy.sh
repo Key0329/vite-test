1ab9cbf777c4db19f29c6af9b524146639bf822f
Key <springfield0329@gmail.com>
2023-01-12 23:37:01 +0800

    feat: add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://key0329.github.io
+# git push -f git@github.com:key0329/key0329.github.io.git main
+
+# if you are deploying to https://key0329.github.io/vite-test
+# git push -f git@github.com:key0329/vite-test.git main:gh-pages
+git push -f https://github.com/Key0329/vite-test.git main:gh-pages
+
+cd -
